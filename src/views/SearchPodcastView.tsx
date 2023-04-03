dbadb1d35c9a1703b3dd3676fbefa633e9f498e6
Jorge Ferrando <jorgeparent@gmail.com>
2023-04-03 19:11:07 +0200

    add search bar

--- src/views/SearchPodcastView.tsx
+++ src/views/SearchPodcastView.tsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
+import { Fragment } from 'react';
+import './SearchPodcastView.sass';
 export const SearchPodcastView = () => {
     return (
-        <div>
-            <h1>Top podcasts</h1>
+        <Fragment>
+            <section className="search-bar">
+                <div>100</div>
+                <input type="text" />
+            </section>
             <ul>
                 <li><Link to={`/podcast/1`}>Podcast 1</Link></li>
                 <li><Link to={`/podcast/2`}>Podcast 2</Link></li>
                 <li><Link to={`/podcast/3`}>Podcast 3</Link></li>
             </ul>
-        </div>
+        </Fragment>
     )
 }
