dad836b0b6ca59271921e9fddd5c6e8ab973ae3f
Jorge Ferrando <jorgeparent@gmail.com>
2023-04-06 19:08:31 +0200

    test for formatDate, updated and fixed method after testing

--- src/helpers/format.helper.ts
+++ src/helpers/format.helper.ts
@@ -1,14 +1,20 @@
 export const formatDate = (date: Date): string => {
-    return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
-}
-
-export const duration = (milliseconds: number) : string => {
-    const totalSeconds = Math.floor(milliseconds / 1000);
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-  
-    const formattedHours = hours < 10 ? `0${hours}` : hours.toString();
-    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes.toString();
-  
-    return `${formattedHours}:${formattedMinutes}`;
+  if (!date) {
+    return "";
   }
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear().toString();
+  return `${day}/${month}/${year}`;
+};
+
+export const duration = (milliseconds: number): string => {
+  const totalSeconds = Math.floor(milliseconds / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+
+  const formattedHours = hours < 10 ? `0${hours}` : hours.toString();
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes.toString();
+
+  return `${formattedHours}:${formattedMinutes}`;
+};
