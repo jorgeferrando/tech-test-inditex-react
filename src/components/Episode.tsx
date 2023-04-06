e08788e9895c8a2a1aab8468a4f30b816028d527
Jorge Ferrando <jorgeparent@gmail.com>
2023-04-06 18:33:56 +0200

    start test for Episode. Blocked because dompurify doesn't run in node environment, if I have time I will look for an alternative

--- src/components/Episode.tsx
+++ src/components/Episode.tsx
@@ -10,12 +10,12 @@ export const Episode = ({episode} : Props) => {
     const description = replaceNewlinesWithBr(convertUrlsToLinks(episode.description));
     return (
         <section className="episodes">
-            <h2>{episode.trackName}</h2>
+            <h2 data-testid="title">{episode.trackName}</h2>
             <div className="episodes__description"
                  dangerouslySetInnerHTML={{__html:dompurify.sanitize(description)}}>
             </div>
             <audio controls>
-                <source src={episode.episodeUrl} type={`${episode.episodeContentType}/${episode.episodeFileExtension}`} />
+                <source data-testid="audio" src={episode.episodeUrl} type={`${episode.episodeContentType}/${episode.episodeFileExtension}`} />
                 Your browser does not support the audio element.
             </audio>
         </section>
