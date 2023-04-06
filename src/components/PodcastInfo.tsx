bf8ad37b4f6a367eb4d4e9340c5f3e608f36a1ab
Jorge Ferrando <jorgeparent@gmail.com>
2023-04-06 09:55:47 +0200

    PodcastInfo style

--- src/components/PodcastInfo.tsx
+++ src/components/PodcastInfo.tsx
@@ -2,6 +2,8 @@ import {Link} from 'react-router-dom'
 import { useDispatch } from 'react-redux';
 import { selectEpisode } from '../stores/podcast.slice';
 
+import './PodcastInfo.sass'
+
 type PodcastInfoProps = {
     podcastInfo: any;
 };
@@ -10,40 +12,46 @@ function formatDate(date: Date) {
     return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
 }
 
-function duration(miliseconds: number) {
-    const seconds = Math.floor(miliseconds / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    return (hours > 0 ? `${hours}h ` : '') + (minutes > 0 ? `${minutes}m ` : '') + (seconds > 0 ? `${seconds}s` : '');
-}
+function duration(milliseconds: number) : string {
+    const totalSeconds = Math.floor(milliseconds / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+  
+    const formattedHours = hours < 10 ? `0${hours}` : hours.toString();
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes.toString();
+  
+    return `${formattedHours}:${formattedMinutes}`;
+  }
 
 export const PodcastInfo = ({podcastInfo} : PodcastInfoProps) => {
     const dispatch = useDispatch()
     return (
-        <section>
-            <h2>{podcastInfo[0].title}</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Date</th>
-                        <th>Duration</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {podcastInfo && podcastInfo.slice(1).map((episode: any) => {
-                        return (
-                            <tr key={episode.episodeGuid}>
-                                <td onClick={() => dispatch(selectEpisode(episode))}>
-                                    <Link to={`episode/${episode.episodeGuid}`}>{episode.trackName}</Link>
-                                </td>
-                                <td>{formatDate(new Date(episode.releaseDate))}</td>
-                                <td>{duration(episode.trackTimeMillis)}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+        <section className="podcast-info">
+            <h2>{podcastInfo[0].collectionName}</h2>
+            <section className="episodes-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th className="align-left">Title</th>
+                            <th className="align-left">Date</th>
+                            <th className="align-center">Duration</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {podcastInfo && podcastInfo.slice(1).map((episode: any) => {
+                            return (
+                                <tr key={episode.episodeGuid}>
+                                    <td className="align-left" onClick={() => dispatch(selectEpisode(episode))}>
+                                        <Link to={`episode/${episode.episodeGuid}`}>{episode.trackName}</Link>
+                                    </td>
+                                    <td className="align-left">{formatDate(new Date(episode.releaseDate))}</td>
+                                    <td className="align-center">{duration(episode.trackTimeMillis)}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </section>
         </section>
     )
 }
